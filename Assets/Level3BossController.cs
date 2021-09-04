6c676ad3aaaf9a432b5a34878df70d3ac236d115
Lanaparezanin <58230697+Lanaparezanin@users.noreply.github.com>
2021-09-04 02:03:55 +0200

    Zombie can be killed

---
+++ Assets/Level3BossController.cs
@@ -0,0 +1,79 @@
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class Level3BossController : MonoBehaviour
+{
+    public Button button;
+
+    public GameObject ZombiStoji;
+    public GameObject ZombiPada;
+    public GameObject ZombieShotRedDot;
+
+    public GameObject WinScreen;
+
+    public InputField inputField1;
+    int value = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField1.ActivateInputField();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputField1.text != "")
+        {
+            value = int.Parse(inputField1.text);
+            //check if number bigger or smaller than 360 and 0
+        }
+    }
+
+    void TaskOnClick()
+    {
+        if (value == 60)
+        {
+            Killer();
+        }
+        else if (value > 60)
+        {
+            MissedUp();
+        }
+        else if (value < 60)
+        {
+            MissedDown();
+        }
+    }
+
+    void Killer()
+    {
+        //make zombie fall
+        ZombiStoji.SetActive(false);
+        ZombiPada.SetActive(true);
+
+        //put red on zombie and make win screen
+        StartCoroutine(ZombieShot());
+    }
+
+    void MissedUp()
+    {
+        
+    }
+
+    void MissedDown()
+    {
+       
+    }
+
+    private IEnumerator ZombieShot()
+    {
+        yield return new WaitForSeconds(0.1f);
+        ZombieShotRedDot.SetActive(true);
+        yield return new WaitForSeconds(1);
+        WinScreen.SetActive(true);
+    }
+}
